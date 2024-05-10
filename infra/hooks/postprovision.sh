5b5148b9f43047edbc3f227ba9ce1f0cee278fcc
Andre Dewes <31827221+andredewes@users.noreply.github.com>
2024-05-10 13:14:03 +0000

    Fixing postprovision subscription parameter

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -4,4 +4,4 @@ echo  "Building summarizationapi:latest..."
 az acr build --subscription ${AZURE_SUBSCRIPTION_ID} --registry ${AZURE_CONTAINER_REGISTRY_NAME} --image summarizationapi:latest ./src/SummarizationAPI/SummarizationAPI/
 image_name="${AZURE_CONTAINER_REGISTRY_NAME}.azurecr.io/summarizationapi:latest"
 az containerapp update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --image ${image_name}
-az containerapp ingress update --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --target-port 8080
+az containerapp ingress update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --target-port 8080
